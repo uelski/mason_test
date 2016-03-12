c7736e7fdb2fc8ed422af4f0ddc07270fcac7e2b
Sam Vredenburgh <sam.vburgh@gmail.com>
2016-03-12 17:53:50 -0600

    readd scroll style stickers

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -14,4 +14,39 @@ $(document).ready(function(){
     $('.welcome').css('margin-top', 0);
   });
 
+  addStickers();
+  removeStickers();
+
+  $("#music-link").click(function() {
+  scrollTime('music-section')
+});
+
+$("#shows-link").click(function() {
+scrollTime('shows-section')
+});
+
 })//end of document.ready
+
+function removeStickers() {
+  var footerSticker = $('.foot-sticker').clone();
+  $(window).resize(function() {
+    if ($(window).width() <= 768) {
+      $('#cos-sticker').replaceWith(footerSticker);
+    }
+  }).resize();
+}
+
+function addStickers() {
+  var stickers = $('#cos-sticker').clone();
+  $(window).resize(function() {
+    var headerHeight = $('.header').height();
+    if ($(window).width() > 768 && headerHeight < 341) {
+      $('.foot-sticker').replaceWith(stickers);
+    }
+  }).resize();
+}
+
+function scrollTime(did) {
+  var aTag = $("div[id='" + did + "']");
+  $('html,body').animate({scrollTop: aTag.offset().top}, 'slow');
+}
